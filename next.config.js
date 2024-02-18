26e0b867a3f4d7c4a5b577f9358409d4f9b23afa
Astrid <github@astrid.exposed>
2024-02-18 16:20:45 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import("next").NextConfig} */
 const config = {
   async rewrites() {
-    return [
-      {
-        source: '/stats/:match*',
-        destination: `${process.env.ANALYTICS_DOMAIN}/:match*`,
-      },
-    ];
+    return process.env.ANALYTICS_DOMAIN
+      ? [
+          {
+            source: '/stats/:match*',
+            destination: `${process.env.ANALYTICS_DOMAIN}/:match*`,
+          },
+        ]
+      : [];
   },
 };
 
