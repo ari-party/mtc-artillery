41d6a4be2406ba9ef960509fc6b8c34fabdb7638
Astrid <github@astrid.exposed>
2024-02-24 20:05:56 +0100

    strapi motd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
 import Stack from '@mui/joy/Stack';
 import Typography from '@mui/joy/Typography';
 import Head from 'next/head';
 import React from 'react';
+import { getEntry } from 'strapi-rest';
 import { useIsClient } from 'usehooks-ts';
 
 import Page from '@/components/layout/Page';
@@ -10,6 +12,7 @@ import AzimuthValue from '@/components/molecules/configuration/Azimuth';
 import ElevationValue from '@/components/molecules/configuration/Elevation';
 import MapSelection from '@/components/molecules/configuration/Map';
 import ProjectileSelection from '@/components/molecules/configuration/Projectile';
+import ScrollBox from '@/components/molecules/ScrollBox';
 import Canvas from '@/components/organisms/Canvas';
 import { maps, projectiles } from '@/constants';
 import { useDataStore } from '@/stores/data';
@@ -22,15 +25,31 @@ import {
 import type { GetStaticPropsResult, InferGetStaticPropsType } from 'next';
 
 export async function getStaticProps(): Promise<
-  GetStaticPropsResult<{ version: string }>
+  GetStaticPropsResult<{ version: string; motd: string | null }>
 > {
   const version = (process.env.VERCEL_GIT_COMMIT_SHA ?? 'dev').slice(0, 9);
+  let motd;
 
-  return { props: { version } };
+  if (process.env.STRAPI_URL && process.env.STRAPI_TYPE) {
+    try {
+      const entry = await getEntry({
+        apiUrl: process.env.STRAPI_URL as string,
+        id: process.env.STRAPI_TYPE as string,
+      });
+
+      if (entry.attributes.text) motd = entry.attributes.text as string;
+    } catch (_) {
+      /* empty */
+      // Don't handle error
+    }
+  }
+
+  return { props: { version, motd: motd || null }, revalidate: 120 };
 }
 
 export default function Index({
   version,
+  motd,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const isClient = useIsClient();
   const [mapIndex, setMapIndex] = useDataStore((s) => [
@@ -65,6 +84,35 @@ export default function Index({
       </Head>
 
       <Page version={version}>
+        {motd && (
+          <Card
+            role="alert"
+            sx={{
+              marginBottom: 2,
+              paddingX: 2,
+              maxHeight: '75px',
+              overflow: 'auto',
+              width: '450px',
+            }}
+            size="sm"
+            variant="soft"
+            orientation="horizontal"
+          >
+            <ScrollBox dependency={motd}>
+              <Typography
+                sx={{
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'wrap',
+                  overflowWrap: 'break-word',
+                  height: 'fit-content',
+                }}
+              >
+                {motd}
+              </Typography>
+            </ScrollBox>
+          </Card>
+        )}
+
         {isClient && (
           <Stack gap={2.5} width={450}>
             <Box
